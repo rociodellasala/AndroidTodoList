395e932904d4837372ec5eadec79df880def9310
Rocío Della Sala <rdella@itba.edu.ar>
2021-03-22 23:24:09 -0300

    set up files

---
+++ app/src/main/java/com/example/pam_project/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pam_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SharedPreferences sharedPref = getSharedPreferences("app-pref",MODE_PRIVATE);
+
+        if(sharedPref.getBoolean("welcome", true)){
+            sharedPref.edit().putBoolean("welcome",false).apply();
+            startActivity(new Intent(this, WelcomeActivity.class));
+        }
+
+    }
+}
