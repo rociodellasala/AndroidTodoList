5329d98bae1f5f806bbfd73ca3c0aaaa45a09776
Rocío Della Sala <rdella@itba.edu.ar>
2021-04-20 00:29:24 -0300

    minor fix create list

--- app/src/main/java/com/example/pam_project/lists/lists/CreateListActivity.java
+++ app/src/main/java/com/example/pam_project/lists/lists/CreateListActivity.java
@@ -2,6 +2,8 @@ package com.example.pam_project.lists.lists;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.pam_project.R;
+import com.example.pam_project.db.AppDatabase;
+import com.example.pam_project.db.entities.CategoryEntity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,27 +14,54 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class CreateListActivity extends AppCompatActivity {
 
     private SpinnerActivity spinnerActivity;
+    private Map<String, Integer> categories;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = AppDatabase.getInstance(getApplicationContext());
+        categories = new HashMap<>();
+
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.activity_title_create_list);
         setContentView(R.layout.activity_create_list);
-        setup();
+
+        getAllCategories();
     }
 
-    private void setup(){
-        // Must be populated with real categories
-        String[] categories = {getResources().getString(R.string.default_category)};
+    private void getAllCategories() {
+        db.categoryDao().getAllCategories()
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(model -> {
+                    setup(model);
+                });
+    }
+
+
+    private void setup(List<CategoryEntity> model){
+        String[] categoriesNames = new String[model.size()];
+
+        for(int i = 0; i < model.size(); i++) {
+            CategoryEntity category = model.get(i);
+            categoriesNames[i] = category.name;
+            categories.put(category.name, category.id);
+        }
+
         Spinner spinner = findViewById(R.id.create_list_category_spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
-                categories);
+                categoriesNames);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinnerActivity = new SpinnerActivity();
@@ -53,10 +82,10 @@ public class CreateListActivity extends AppCompatActivity {
 
         if (id == R.id.check_add_button) {
             String listTile = listTitleInput.getText().toString();
-            String listCategory = spinner.getSelectedItem().toString();
+            Integer categoryId = categories.get(spinner.getSelectedItem().toString());
             Intent returnIntent = new Intent();
             returnIntent.putExtra("listTile", listTile);
-            returnIntent.putExtra("listCategory", listCategory);
+            returnIntent.putExtra("categoryId", categoryId.toString());
             setResult(Activity.RESULT_OK, returnIntent);
             finish();
         }
