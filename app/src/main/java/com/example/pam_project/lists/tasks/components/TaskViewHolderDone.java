df6020cc1e79bc92216781d83c67b9f797b7579b
Rocío Della Sala <rdella@itba.edu.ar>
2021-05-15 00:28:43 -0300

    Add abstract class for task view holder

--- app/src/main/java/com/example/pam_project/lists/tasks/components/TaskViewHolderDone.java
+++ app/src/main/java/com/example/pam_project/lists/tasks/components/TaskViewHolderDone.java
@@ -1,35 +1,13 @@
 package com.example.pam_project.lists.tasks.components;
 
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.pam_project.R;
-import com.example.pam_project.lists.lists.listActivity.OnListClickedListener;
-
-public class TaskViewHolderDone extends RecyclerView.ViewHolder {
-
-    private OnListClickedListener listener;
+public class TaskViewHolderDone extends TaskViewHolder {
 
     public TaskViewHolderDone(@NonNull View itemView) {
         super(itemView);
-    }
-
-    public void bind(final TaskInformation task) {
-        final TextView title = itemView.findViewById(R.id.title);
-        title.setText(task.getTitle());
-
-        itemView.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onClick(task.getId());
-            }
-        });
-    }
-
-    public void setOnClickListener(final OnListClickedListener listener) {
-        this.listener = listener;
     }
 
 }
--- app/src/main/java/com/example/pam_project/lists/tasks/components/TaskViewHolderPending.java
+++ app/src/main/java/com/example/pam_project/lists/tasks/components/TaskViewHolderPending.java
@@ -10,32 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.pam_project.R;
 import com.example.pam_project.lists.lists.listActivity.OnListClickedListener;
 
-public class TaskViewHolderPending extends RecyclerView.ViewHolder {
-
-    private OnListClickedListener listener;
+public class TaskViewHolderPending  extends TaskViewHolder {
 
     public TaskViewHolderPending(@NonNull View itemView) {
         super(itemView);
     }
 
     public void bind(final TaskInformation task) {
-        final TextView title = itemView.findViewById(R.id.title);
+        super.bind(task);
         final TextView description = itemView.findViewById(R.id.description);
-        title.setText(task.getTitle());
         description.setText(task.getDescription());
         if (task.getUrgency()) {
             final ImageView urgency = itemView.findViewById(R.id.urgency);
             urgency.setVisibility(View.VISIBLE);
         }
-
-        itemView.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onClick(task.getId());
-            }
-        });
-    }
-
-    public void setOnClickListener(final OnListClickedListener listener) {
-        this.listener = listener;
     }
 }
