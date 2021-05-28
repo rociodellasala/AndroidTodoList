9d08eddd5165fe1f9a9a43ae6bcb59b556bcd9b8
Rocío Della Sala <rdella@itba.edu.ar>
2021-05-27 23:35:02 -0300

    Add fix bug in task

--- app/src/main/java/com/example/pam_project/lists/tasks/components/CustomItemDecorator.java
+++ app/src/main/java/com/example/pam_project/lists/tasks/components/CustomItemDecorator.java
@@ -13,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pam_project.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CustomItemDecorator extends RecyclerView.ItemDecoration {
@@ -41,7 +43,7 @@ public class CustomItemDecorator extends RecyclerView.ItemDecoration {
             View view = parent.getChildAt(i);
             int position = parent.getChildAdapterPosition(view);
             int viewType = Objects.requireNonNull(parent.getAdapter()).getItemViewType(position);
-            if (viewType != currentViewType) {
+            if (viewType != currentViewType && j < allHeaders.length) {
                 mLayout[j].layout(parent.getLeft(), 0, parent.getRight(), mLayout[j].getMeasuredHeight());
                 textView[j].setVisibility(View.VISIBLE);
                 textView[j].setY(view.getTop() - mLayout[j].getMeasuredHeight());
@@ -54,7 +56,7 @@ public class CustomItemDecorator extends RecyclerView.ItemDecoration {
 
     @Override
     public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
-        String[] headers = updatePendingHeaders(parent);
+        List<String> headers = updatePendingHeaders(parent);
         updateLayout(parent, headers);
         int currentViewType = -1;
 
@@ -62,7 +64,7 @@ public class CustomItemDecorator extends RecyclerView.ItemDecoration {
             View child = parent.getChildAt(i);
             int position = parent.getChildAdapterPosition(child);
             int viewType = Objects.requireNonNull(parent.getAdapter()).getItemViewType(position);
-            if (viewType != currentViewType) {
+            if (viewType != currentViewType && j < headers.size()) {
                 outRect.set(0, mLayout[j].getMeasuredHeight(), 0, 0);
                 currentViewType = viewType;
                 j++;
@@ -72,33 +74,34 @@ public class CustomItemDecorator extends RecyclerView.ItemDecoration {
         }
     }
 
-    private void updateLayout(RecyclerView parent, String[] headers) {
-        for (int i = 0; i < headers.length; i++) {
+    private void updateLayout(RecyclerView parent, List<String> headers) {
+        for (int i = 0; i < headers.size(); i++) {
             mLayout[i] = LayoutInflater.from(context).inflate(resId, parent, false);
             textView[i] = mLayout[i].findViewById(R.id.recycler_header);
-            textView[i].setText(headers[i]);
+            textView[i].setText(headers.get(i));
             textView[i].setVisibility(View.INVISIBLE);
             mLayout[i].measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
                     View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
         }
     }
 
-    private String[] updatePendingHeaders(RecyclerView parent) {
-        String[] updatedHeaders = new String[allHeaders.length];
+    private List<String> updatePendingHeaders(RecyclerView parent) {
+        List<String> updatedHeaders = new ArrayList<>();
 
         int currentViewType = -1;
 
-        for (int i = 0, j = 0; i < parent.getChildCount(); i++) {
+        for (int i = 0; i < parent.getChildCount(); i++) {
             View view1 = parent.getChildAt(i);
             int position = parent.getChildAdapterPosition(view1);
             int viewType = Objects.requireNonNull(parent.getAdapter()).getItemViewType(position);
             if (viewType != currentViewType) {
                 if (viewType == TASK_PENDING) {
-                    updatedHeaders[j] = allHeaders[TASK_PENDING];
+                    if(!updatedHeaders.contains(allHeaders[TASK_PENDING]))
+                        updatedHeaders.add(allHeaders[TASK_PENDING]);
                 } else {
-                    updatedHeaders[j] = allHeaders[TASK_DONE];
+                    if(!updatedHeaders.contains(allHeaders[TASK_DONE]))
+                        updatedHeaders.add(allHeaders[TASK_DONE]);
                 }
-                j++;
                 currentViewType = viewType;
             }
         }
