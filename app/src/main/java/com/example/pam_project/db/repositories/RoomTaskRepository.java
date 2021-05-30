995434d11dd0f6d7a0881959890c7395e27296a2
arodriguez1415 <arodriguez@itba.edu.ar>
2021-05-30 01:26:20 -0300

    task mapper deleted and status as enum

--- app/src/main/java/com/example/pam_project/db/repositories/RoomTaskRepository.java
+++ app/src/main/java/com/example/pam_project/db/repositories/RoomTaskRepository.java
@@ -3,7 +3,6 @@ package com.example.pam_project.db.repositories;
 import com.example.pam_project.db.daos.TaskDao;
 import com.example.pam_project.db.entities.TaskEntity;
 import com.example.pam_project.lists.tasks.components.TaskInformation;
-import com.example.pam_project.utils.StatusMapper;
 import com.example.pam_project.utils.TaskStatus;
 
 public class RoomTaskRepository implements TaskRepository {
@@ -18,20 +17,20 @@ public class RoomTaskRepository implements TaskRepository {
     public TaskInformation getTask(final long taskId) {
         TaskEntity taskEntity = taskDao.getTaskById(taskId).blockingFirst();
         return new TaskInformation(taskEntity.id, taskEntity.name, taskEntity.description,
-                taskEntity.priority, StatusMapper.toTaskStatusMapper(taskEntity.status));
+                taskEntity.priority, TaskStatus.getStatus(taskEntity.status));
     }
 
     @Override
     public long insertTask(final String name, final String description, final boolean priority,
                            final TaskStatus status, final long listId) {
-        TaskEntity taskEntity = new TaskEntity(name, description, priority, StatusMapper.toStringStatus(status), listId);
+        TaskEntity taskEntity = new TaskEntity(name, description, priority, TaskStatus.statusToString(status), listId);
         return taskDao.insertTask(taskEntity);
     }
 
     @Override
     public void updateTask(final long id, final String name, final String description, final boolean priority,
                            final TaskStatus status, final long listId) {
-        TaskEntity taskEntity = new TaskEntity(id, name, description, priority, StatusMapper.toStringStatus(status), listId);
+        TaskEntity taskEntity = new TaskEntity(id, name, description, priority, TaskStatus.statusToString(status), listId);
         taskDao.updateTask(taskEntity);
     }
 
