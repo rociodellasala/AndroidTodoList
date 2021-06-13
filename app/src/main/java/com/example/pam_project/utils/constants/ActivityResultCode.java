a9ce765b36c45e7a96475588b381a27901d5128b
Rocío Della Sala <rdella@itba.edu.ar>
2021-06-13 20:37:41 -0300

    Remove enums (#77)

--- app/src/main/java/com/example/pam_project/utils/constants/ActivityResultCode.java
+++ app/src/main/java/com/example/pam_project/utils/constants/ActivityResultCode.java
@@ -1,13 +1,5 @@
 package com.example.pam_project.utils.constants;
 
 public enum ActivityResultCode {
-    CREATE_LIST_CODE,
-    EDIT_LIST_CODE,
-    DELETE_LIST_CODE,
-    CREATE_CATEGORY_CODE,
-    EDIT_CATEGORY_CODE,
-    DELETE_CATEGORY_CODE,
-    CREATE_TASK_CODE,
-    EDIT_TASK_CODE,
-    DELETE_TASK_CODE,
+    DELETE_LIST_CODE
 }
