453d42e0027ea31c4f06c36ef2af43f059fb2dfd
Rocío Della Sala <rdella@itba.edu.ar>
2021-07-16 20:32:11 -0300

    comparators fixes

--- app/src/main/java/com/example/pam_project/comparators/ListAlphabeticalComparator.kt
+++ app/src/main/java/com/example/pam_project/comparators/ListAlphabeticalComparator.kt
@@ -4,7 +4,7 @@ import com.example.pam_project.features.lists.list.ListInformation
 import java.lang.String
 
 class ListAlphabeticalComparator : ListInformationComparator() {
-    override fun compare(o1: ListInformation, o2: ListInformation): Int {
-        return String.CASE_INSENSITIVE_ORDER.compare(o1.title, o2.title)
+    override fun compare(o1: ListInformation?, o2: ListInformation?): Int {
+        return String.CASE_INSENSITIVE_ORDER.compare(o1?.title, o2?.title)
     }
 }
--- app/src/main/java/com/example/pam_project/comparators/ListDateAddedComparator.kt
+++ app/src/main/java/com/example/pam_project/comparators/ListDateAddedComparator.kt
@@ -3,9 +3,9 @@ package com.example.pam_project.comparators
 import com.example.pam_project.features.lists.list.ListInformation
 
 class ListDateAddedComparator : ListInformationComparator() {
-    override fun compare(o1: ListInformation, o2: ListInformation): Int {
+    override fun compare(o1: ListInformation?, o2: ListInformation?): Int {
         if (o1 === o2) return 0
         val nullCmp = nullCompare(o1, o2)
-        return if (nullCmp != 0) nullCmp else java.lang.Long.compare(o1.id, o2.id)
+        return if (nullCmp != 0) nullCmp else o1!!.id.compareTo(o2!!.id)
     }
 }
